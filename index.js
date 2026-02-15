a77c03b37e6fe2cf26a6a52b2625276f30b135a1
Kelly C Santos <kellycristinasntos@gmail.com>
2026-02-15 17:43:48 -0300

    feat: created products api

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express()
+const PORT = 3001
+
+const products = require('./products.json')
+
+
+app.get('/products', (req,res) => {
+    const {name, category} = req.query;
+    let filterByCategory = products;
+
+        if(category){
+            filterByCategory = products.filter((products) => {
+                return products.category === category.toLowerCase() 
+            })
+        }
+
+        if(name){
+            filterByCategory = filterByCategory.filter((products) => {
+                return products.name.includes(name.toLowerCase()) || products.namept.includes(name.toLowerCase())
+            })
+        }
+    res.json(filterByCategory)
+})
+
+app.get('/product/:id', (req, res) => {
+    const {id} = req.params
+    const filtered = products.filter((item) => {return item.id == id})
+    res.json(filtered)
+})
+
+
+
+app.listen(PORT, () => {
+    console.log('Servidor online na porta', PORT)
+})
