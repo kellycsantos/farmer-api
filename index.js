786ead2c8ead94b715f925b3e00ba6431bf09acf
Kelly C Santos <kellycristinasntos@gmail.com>
2026-02-20 01:25:44 -0300

    feat: add status code and find function

--- index.js
+++ index.js
@@ -7,28 +7,47 @@ const products = require('./products.json')
 
 app.use(cors())
 
-app.get('/products', (req,res) => {
-    const {name, category} = req.query;
+app.get('/products', (req, res) => {
+    const { name, category } = req.query;
     let filterByCategory = products;
 
-        if(category){
-            filterByCategory = products.filter((products) => {
-                return products.category === category.toLowerCase() 
-            })
-        }
+    if (category) {
+        filterByCategory = products.filter((products) => {
+            return products.category === category.toLowerCase()
+        })
+    }
 
-        if(name){
-            filterByCategory = filterByCategory.filter((products) => {
-                return products.name.includes(name.toLowerCase()) || products.namept.includes(name.toLowerCase())
-            })
-        }
-    res.json(filterByCategory)
+    if (name) {
+        filterByCategory = filterByCategory.filter((products) => {
+            return products.name.includes(name.toLowerCase()) || products.namept.includes(name.toLowerCase())
+        })
+    }
+    res.status(200).json(filterByCategory)
 })
 
-app.get('/product/:id', (req, res) => {
-    const {id} = req.params
-    const filtered = products.filter((item) => {return item.id == id})
-    res.json(filtered)
+app.get('/product/id/:id', (req, res) => {
+    const { id } = req.params
+    const filtered = products.find((item) => { return item.id == id })
+    if (!filtered) {
+        return res.status(404).json({
+            message: "Product not found."
+        })
+    }
+    res.status(200).json(filtered)
+
+})
+app.get('/product/:sku', (req, res) => {
+    const { sku } = req.params
+    const filtered = products.find((item) => {
+        return item.sku == sku
+    })
+    if (!filtered) {
+        return res.status(404).json({
+            message: "Product not found."
+        })
+    }
+    res.status(200).json(filtered)
+
 })
 
 
